2f1a7ee0f1509f34892068f30b683c46eaa65e43
BENIE <sylvestrebenie7829@gmail.com>
2023-06-01 10:10:34 +0000

    creation des differentes pages html

--- src/kowodieBblog/views.py
+++ src/kowodieBblog/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render 
+# from django.http import HttpResponse
 
-def home_view(request):
-    return HttpResponse('<h2>Nous sommes les meilleurs</h2>')
+def home_view(request): 
+    return render(request, "home.html")
+
+def contact_view(request):
+    return render(request, "contact.html")
+
+
+def article_view(request):
+    return render(request, "article.html")
