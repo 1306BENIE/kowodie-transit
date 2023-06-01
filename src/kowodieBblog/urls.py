b805c6f7cbe97288ec558b6e8f87103ebf7fab0a
BENIE <sylvestrebenie7829@gmail.com>
2023-06-01 16:09:16 +0000

    ajout des fichier

--- src/kowodieBblog/urls.py
+++ src/kowodieBblog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/admin/', admin.site.urls),
     path('', views.home_view),
     path('contact/', views.contact_view),
     path('article/', views.article_view),
